03faeeaad2ef4b7b50c82678afe0e731c1ae8622
Lazyihui <chengbaozi6@qq.com>
2025-10-09 20:36:34 +0800

    <feature>Add:ESTABLISH

---
+++ Assets/Src_Runtime/ClientMain.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientMain : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("ClientMain Start");
+    }
+
+    void Update()
+    {
+
+    }
+}
